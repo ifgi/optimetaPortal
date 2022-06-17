9382084ff2cbdb808f776741f4a4763bbb05bcb4
Kirubamoh <nkirubamohan@gmail.com>
2022-06-17 09:33:23 +0200

    updated url to api

--- geodjango/markers/static/map.js
+++ geodjango/markers/static/map.js
@@ -67,3 +67,18 @@ var geojsonFeature = {
 L.geoJSON(geojsonFeature, {
     onEachFeature: onEachFeature
 }).addTo(map);
+
+async function load_markers() {
+    const markers_url = `/api/markers/?in_bbox=${map.getBounds().toBBoxString()}`
+    const response = await fetch(markers_url)
+    const geojson = await response.json()
+    return geojson
+}
+
+async function render_markers() {
+    const markers = await load_markers();
+    L.geoJSON(markers)   
+    .addTo(map);
+}
+
+map.on("moveend", render_markers);
