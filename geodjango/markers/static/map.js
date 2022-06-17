4084ef70159a5f0e59e9fc495d6c5c0eda535c38
Kirubamoh <nkirubamohan@gmail.com>
2022-06-17 08:14:46 +0200

    Add geojson to map and popup

--- geodjango/markers/static/map.js
+++ geodjango/markers/static/map.js
@@ -1,5 +1,69 @@
 const copy = "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors";
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [osm] });
+const map = L.map("map", { layers: [osm] }).setView([39.74739, -105], 5);
 map.fitWorld();
+
+
+/*popup*/
+function onEachFeature(feature, layer) {
+    var popupContent = 'shows the title of artcile ' +
+        '<a href="http://www.google.com"><h1> Visit Article</h1></a>' ;
+
+    if (feature.properties && feature.properties.popupContent) {
+        popupContent += feature.properties.popupContent;
+    }
+
+    layer.bindPopup(popupContent);
+}
+
+/*geometry*/
+var geojsonFeature = [{
+    "type": "Feature",
+    "properties": {
+        "name": "iceland"       
+    },
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+},{
+    "type": "Feature",
+    "properties": {
+        "name": "ireland"       
+    },
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+/*
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};*/
+
+/*add geojson to map*/
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
