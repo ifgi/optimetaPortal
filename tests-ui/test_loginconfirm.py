355cd92759e446ac60c4b16c57493e53f76bb129
Kirubamoh <nkirubamohan@gmail.com>
2022-10-28 04:41:33 +0200

    Add UI tests for new pages

---
+++ tests-ui/test_loginconfirm.py
@@ -0,0 +1,12 @@
+import unittest
+import os
+from helium import *
+
+class LoginconfirmationTest(unittest.TestCase):
+    
+    start_firefox('localhost:8000/loginconfirm/')  
+    get_driver().save_screenshot(os.path.join(os.getcwd(), 'tests-ui/screenshots/UserMenu.png'))
+    if Text("Welcome to Optimeta!").exists():
+        click(Button("×"))
+    kill_browser()
+    
---
+++ tests-ui/test_usermenu.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from helium import *
+
+
+
+
+class UsermenuTest(unittest.TestCase):
+    
+    start_firefox('localhost:8000/')    
+    click(S("#navbarDarkDropdown"))
+    get_driver().save_screenshot(os.path.join(os.getcwd(), 'tests-ui/screenshots/UserMenu.png'))
+    kill_browser()
