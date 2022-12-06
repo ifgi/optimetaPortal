da68192ac30c6f95ccf5f61acab1855668285cec
Kirubamoh <nkirubamohan@gmail.com>
2022-12-06 08:42:24 +0100

    rework harvest function

--- publications/tasks.py
+++ publications/tasks.py
@@ -1,27 +1,73 @@
 from django_q.models import Schedule
-from datetime import date, timezone
 from publications.models import Publication
-from django.core.mail import send_mail
+from views import harvest_data
+import requests
+from bs4 import BeautifulSoup
+import json
+import xml.dom.minidom
+from xml.dom.minidom import parseString
+from django.db.models import Max
+from django.contrib.gis.geos import GEOSGeometry
 
+def harvest_demo():
+    url1 = 'https://service.tib.eu/optimeta/index.php/optimeta/oai/?verb=ListRecords&metadataPrefix=oai_dc'
+    harvest_data(url1)
+
+def get_geom(url):
+    #soup = BeautifulSoup(open(url,encoding="utf8"), "html.parser") # forlocal file
+    req= requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    for tag in soup.find_all("meta"):
+        if tag.get("name", None) == "DC.SpatialCoverage":
+            data = tag.get("content", None)
+            json_object = json.loads(data)
+            return json_object
+
+
+def harvest_data(url):
+    # url1 = os.path.join(os.getcwd(),'harvesting\\journal_1\\oai_dc.xml') # forlocalfile
+    response = requests.get(url)
+    DOMTree = xml.dom.minidom.parseString(response.content)# parse xml as DOM
+    #DOMTree = xml.dom.minidom.parse(url1) 
+    collection = DOMTree.documentElement
+    articles = collection.getElementsByTagName("dc:identifier")                                            
+    no_articles = len(articles)  # number of articles in journal
+    for i in range(no_articles):
+        identifier = collection.getElementsByTagName("dc:identifier")
+        identifier_value = identifier[i].firstChild.nodeValue
+        if identifier_value.startswith('https://'):
+            link_value = identifier_value               
+            #get geometry from html
+            #identifier_url = os.path.join(os.getcwd(),'harvesting\\journal_1\\article_01.html')
+            geom = get_geom(link_value)
+            geom_data = geom["features"][0]["geometry"]
+            geom_data_string = json.dumps(geom_data)
+            # preparing geometry data in accordance to geosAPI fields
+            type_geom= {'type': 'GeometryCollection'}
+            geom_content = {"geometries" : [geom_data_string]}
+            type_geom.update(geom_content)
+            geom_data = json.dumps(type_geom)
+            # geometry field accept json object as string
+            geom_object = GEOSGeometry(geom_data) #GeometryCollection object
+            #bounds=MultiPolygon([Polygon(((-117.869537353516, 33.5993881225586),(-117.869537353516, 33.7736549377441),(-117.678024291992, 33.7736549377441),(-117.678024291992, 33.5993881225586),(-117.869537353516, 33.5993881225586)))])           
+            #gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), bounds)
+            title = collection.getElementsByTagName("dc:title")
+            title_value = title[0].firstChild.nodeValue
+            abstarct = collection.getElementsByTagName("dc:description") 
+            abstarct_text = abstarct[0].firstChild.nodeValue
+            journal = collection.getElementsByTagName("dc:publisher")
+            journal_value = journal[0].firstChild.nodeValue
+            date = collection.getElementsByTagName("dc:date")
+            date_value = date[0].firstChild.nodeValue 
+            #get latest id from table
+            h = Publication.objects.all()
+            max = h.aggregate(Max('id'))
+            j = max["id__max"] + 1
+            publication = Publication(id = i+j ,title = title_value,abstract = abstarct_text,publicationDate = date_value, url = link_value , geometry = geom_object,journal = journal_value)
+            publication.save()
+
+# shell
 Schedule.objects.create(
-    func='publications.views.get_info',
-    schedule_type=Schedule.DAILY
-)
-
-def new_manuscript_updates():
-    """
-    send email for manuscript updates
-    """
-    one_minute_ago = timezone.now() - timezone.timedelta(minutes=1)
-    new_manuscripts = Publication.objects.filter(
-        date = one_minute_ago
-    )
-    subject = 'New Manuscripts'
-    email = "test@example.com"
-    message =f"""\ Hello, we send you updated manuscripts that you were looking for:    {new_manuscripts}   """
-    send_mail(subject, message, from_email= "optimetageo@gmail.com",recipient_list=[email])
-
-Schedule.objects.create(
-    func='publications.tasks.new_manuscript_updates',
+    func= 'publications.tasks.harvest_demo',
     schedule_type=Schedule.MONTHLY
 )
--- publications/views.py
+++ publications/views.py
@@ -9,13 +9,12 @@ from django.http.request import HttpRequest
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 from .forms import LoginForm
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.core.mail import send_mail, BadHeaderError
-from django.core.cache import cache
 import secrets
 import requests
 from django.contrib import messages
-from django.contrib.gis.geos import Polygon,MultiPolygon,GeometryCollection,Point,MultiPoint
+from django.contrib.gis.geos import Polygon,MultiPolygon,GEOSGeometry
 from django.core import signing
 from django.contrib.auth import login, get_user_model,logout
 from django.views.decorators.http import require_GET
@@ -27,8 +26,8 @@ from django.conf import settings
 from bs4 import BeautifulSoup
 import json
 import os
-import xml.dom.minidom
-from xml.dom.minidom import parse
+from django.db.models import Max
+
 
 
 #populate database from datacite
@@ -95,9 +94,9 @@ def successView(request):
     return HttpResponse("Success! We sent a log in link. Check your email.")
 
 def optimap(request):
-	return render(request,"main.html")
+    return render(request,"main.html")
+    
    
-
 def loginres(request):
     
     email = request.POST.get('email', False)    
@@ -109,6 +108,7 @@ def loginres(request):
     message =f"""Hello,You requested that we send you a link to log in to our app:    {link} .Please click on the link to login."""
     send_mail(subject, message, from_email= settings.EMAIL_HOST_USER,recipient_list=[email])
     return render(request,'login_response.html')
+    
     
 
 def privacypolicy(request):
@@ -167,41 +167,61 @@ def change_useremail(request):
 
     return  render(request,'changeuser.html')
 
+def cuser(request):
+    return render(request,'changeuser.html')
+
 def get_geom(url):
-    soup = BeautifulSoup(open(url,encoding="utf8"), "html.parser") #local file
+    #soup = BeautifulSoup(open(url,encoding="utf8"), "html.parser") # forlocal file
+    req= requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
     for tag in soup.find_all("meta"):
         if tag.get("name", None) == "DC.SpatialCoverage":
             data = tag.get("content", None)
             json_object = json.loads(data)
             return json_object
 
-
-def harvest_data(request):
-    url1 = os.path.join(os.getcwd(),'harvesting\\journal_1\\oai_dc.xml')
-    DOMTree = xml.dom.minidom.parse(url1) # parse xml as DOM
+import xml.dom.minidom
+from xml.dom.minidom import parse
+def harvest_data(url):
+    # url1 = os.path.join(os.getcwd(),'harvesting\\journal_1\\oai_dc.xml') # forlocalfile
+    response = requests.get(url)
+    DOMTree = xml.dom.minidom.parseString(response.content)# parse xml as DOM
+    #DOMTree = xml.dom.minidom.parse(url1) 
     collection = DOMTree.documentElement
-    articles = collection.getElementsByTagName("record")                                            
+    articles = collection.getElementsByTagName("dc:identifier")                                            
     no_articles = len(articles)  # number of articles in journal
     for i in range(no_articles):
-        records = collection.getElementsByTagName("record")[i]
-        identifier = records.getElementsByTagName("identifier")
-        identifier_value = identifier[0].firstChild.nodeValue
-        #get geometry from html
-        #identifier_url = os.path.join(os.getcwd(),'harvesting\\journal_1\\article_01.html')
-        #geom = get_geom(identifier_url)
-        #geom_data = geom["features"][0]["geometry"]
-        #geom_data_string = json.dumps(geom_data)
-        bounds=MultiPolygon([Polygon(((-117.869537353516, 33.5993881225586),(-117.869537353516, 33.7736549377441),(-117.678024291992, 33.7736549377441),(-117.678024291992, 33.5993881225586),(-117.869537353516, 33.5993881225586)))])           
-        gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), bounds)
-        title = records.getElementsByTagName("dc:title")
-        title_value = title[0].firstChild.nodeValue
-        journal = records.getElementsByTagName("dc:publisher")
-        journal_value = journal[0].firstChild.nodeValue
-        date = records.getElementsByTagName("dc:date")
-        date_value = date[0].firstChild.nodeValue
-        link = records.getElementsByTagName("dc:identifier")
-        link_value = link[0].firstChild.nodeValue
-        publication = Publication(id = i+2 ,title = title_value,publicationDate = date_value, url = link_value , geometry = gc)
-        publication.save()
+        identifier = collection.getElementsByTagName("dc:identifier")
+        identifier_value = identifier[i].firstChild.nodeValue
+        if identifier_value.startswith('https://'):
+            link_value = identifier_value               
+            #get geometry from html
+            #identifier_url = os.path.join(os.getcwd(),'harvesting\\journal_1\\article_01.html')
+            geom = get_geom(link_value)
+            geom_data = geom["features"][0]["geometry"]
+            geom_data_string = json.dumps(geom_data)
+            # preparing geometry data in accordance to geosAPI fields
+            type_geom= {'type': 'GeometryCollection'}
+            geom_content = {"geometries" : [geom_data_string]}
+            type_geom.update(geom_content)
+            geom_data = json.dumps(type_geom)
+            # geometry field accept json object as string
+            geom_object = GEOSGeometry(geom_data) #GeometryCollection object
+            #bounds=MultiPolygon([Polygon(((-117.869537353516, 33.5993881225586),(-117.869537353516, 33.7736549377441),(-117.678024291992, 33.7736549377441),(-117.678024291992, 33.5993881225586),(-117.869537353516, 33.5993881225586)))])           
+            #gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), bounds)
+            title = collection.getElementsByTagName("dc:title")
+            title_value = title[0].firstChild.nodeValue
+            abstarct = collection.getElementsByTagName("dc:description") 
+            abstarct_text = abstarct[0].firstChild.nodeValue
+            journal = collection.getElementsByTagName("dc:publisher")
+            journal_value = journal[0].firstChild.nodeValue
+            date = collection.getElementsByTagName("dc:date")
+            date_value = date[0].firstChild.nodeValue 
+            #get latest id from table
+            h = Publication.objects.all()
+            max = h.aggregate(Max('id'))
+            j = max["id__max"] + 1
+            publication = Publication(id = i+j ,title = title_value,abstract = abstarct_text,publicationDate = date_value, url = link_value , geometry = geom_object,journal = journal_value)
+            publication.save()
     
-    return render(request,'subscriptions.html')
+    
