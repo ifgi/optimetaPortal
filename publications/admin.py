afed662e1a5db066180f4e5684571e8ec3c12e8f
Kirubamoh <nkirubamohan@gmail.com>
2022-12-06 08:47:30 +0100

    add new model for ojsservers

--- publications/admin.py
+++ publications/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from django.contrib.gis import admin
 
-from publications.models import Publication
+from publications.models import Publication,OJSservers
 
 
 @admin.register(Publication)
@@ -12,3 +12,8 @@ class PublicationAdmin(admin.OSMGeoAdmin):
 
     list_display = ("title", "publicationDate", "creationDate", "lastUpdate")
 # Register your models here.
+
+@admin.register(OJSservers)
+class ServerAdmin(admin.OSMGeoAdmin):
+
+    url_display = ("url_field","Harvest_Interval","Last_Harvest")
