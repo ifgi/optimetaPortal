dc7a763d0f8f57a58e42dc5c06103da1632aa7e4
Kirubamoh <nkirubamohan@gmail.com>
2022-12-09 04:06:57 +0100

    Add models for OJSservers

--- publications/models.py
+++ publications/models.py
@@ -2,23 +2,29 @@ from django.db import models
 """Markers models."""
 
 from django.contrib.gis.db import models
-from datetime import date
+
 
 
 class Publication(models.Model):
-    """A scientific publication with a title and location."""
-
+    """A marker with name and location."""
+       
     title = models.CharField(max_length=4096)
-    abstract = models.TextField()
-    publicationDate = models.DateField()
+    abstract = models.TextField( null=True)
+    publicationDate = models.DateField(auto_now_add=True)
     doi = models.CharField(max_length=1024, null=True)
     url = models.URLField(max_length=1024, null=True)
-    geometry = models.GeometryCollectionField()
+    geometry = models.GeometryCollectionField(verbose_name='geo',srid = 4326, null=True, blank=True)
     journal = models.CharField(max_length=1024, null=True)
-
-    creationDate = models.DateTimeField(auto_now_add=True)
-    lastUpdate = models.DateTimeField(auto_now=True)
+    creationDate = models.DateTimeField(auto_now_add=True,null=True)
+    lastUpdate = models.DateTimeField(auto_now=True,null=True)
 
     def __str__(self):
         """Return string representation."""
         return self.title
+
+class OJSservers(models.Model):
+
+    url_field = models.URLField(max_length = 200)
+    harvest_interval = models.DurationField(null=True)
+    last_harvest = models.DateTimeField(auto_now_add=True,null=True)
+    
