56bb6b0d2f8edb39165545298bffe937f92f7f01
Kirubamoh <nkirubamohan@gmail.com>
2022-12-14 03:42:46 +0100

    Add subscription model and update fields

--- publications/models.py
+++ publications/models.py
@@ -2,7 +2,7 @@ from django.db import models
 """Markers models."""
 
 from django.contrib.gis.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 
 class Publication(models.Model):
@@ -10,13 +10,15 @@ class Publication(models.Model):
        
     title = models.CharField(max_length=4096)
     abstract = models.TextField( null=True)
-    publicationDate = models.DateField(auto_now_add=True)
+    publicationDate = models.DateField(null=True,Blank = True)
     doi = models.CharField(max_length=1024, null=True)
     url = models.URLField(max_length=1024, null=True)
-    geometry = models.GeometryCollectionField(verbose_name='geo',srid = 4326, null=True, blank=True)
+    geometry = models.GeometryCollectionField(verbose_name='Publication geometry (Points, Lines, Polygons as GeoJSON)',srid = 4326, null=True, blank=True)
     journal = models.CharField(max_length=1024, null=True)
     creationDate = models.DateTimeField(auto_now_add=True,null=True)
     lastUpdate = models.DateTimeField(auto_now=True,null=True)
+    timeperiod_startdate = ArrayField(models.DateField(null=True))
+    timeperiod_enddate = ArrayField(models.DateField(null=True))
 
     def __str__(self):
         """Return string representation."""
@@ -25,6 +27,12 @@ class Publication(models.Model):
 class OJSservers(models.Model):
 
     url_field = models.URLField(max_length = 200)
-    harvest_interval = models.DurationField(null=True)
+    harvest_interval_minutes = models.IntegerField(default=60*24*3)
     last_harvest = models.DateTimeField(auto_now_add=True,null=True)
     
+class Subscription(models.model):
+    search_text = models.CharField(max_length=4096)
+    timeperiod_startdate = models.DateField(null=True)
+    timeperiod_enddate = models.DateField(null=True)
+    search_area = models.GeometryCollectionField(null=True, blank=True)
+    user_name = models.CharField(max_length=4096)
