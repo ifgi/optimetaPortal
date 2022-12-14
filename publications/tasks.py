3f03271973ec259196213e30995d729efca8dec0
Kirubamoh <nkirubamohan@gmail.com>
2022-12-14 03:44:38 +0100

    Update Harvest functions

--- publications/tasks.py
+++ publications/tasks.py
@@ -4,14 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import xml.dom.minidom
-from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.geos import GEOSGeometry 
 
-def harvest_demo():
-    url1 = 'https://service.tib.eu/optimeta/index.php/optimeta/oai/?verb=ListRecords&metadataPrefix=oai_dc'
-    harvest_data(url1)
 
 def get_geom(url):
-    #soup = BeautifulSoup(open(url,encoding="utf8"), "html.parser") # forlocal file
     req= requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
     return parse_html(soup)
@@ -20,25 +16,30 @@ def parse_html(content):
     for tag in content.find_all("meta"):
         if tag.get("name", None) == "DC.SpatialCoverage":
             data = tag.get("content", None)
-            json_object = json.loads(data)
-            return(json_object)
+            try:
+                json_object = json.loads(data)
+                return(json_object)
+            except ValueError as e:
+                print("Not a valid GeoJSON")
         else : 
             pass # return None     
 
-def harvest_data(url):
-    # url1 = os.path.join(os.getcwd(),'harvesting\\journal_1\\oai_dc.xml') # forlocalfile
-    response = requests.get(url)
-    DOMTree = xml.dom.minidom.parseString(response.content)
-    collection = DOMTree.documentElement
+def extract_data_from_article(identifier_value):
+    pass
+    
+
+def parse_xml(content):
+    
+    DOMTree = xml.dom.minidom.parseString(content)
+    collection = DOMTree.documentElement # pass DOMTree as argument
     articles = collection.getElementsByTagName("dc:identifier")                                            
     articles_count_in_journal = len(articles)  # number of articles in journal
     for i in range(articles_count_in_journal):
         identifier = collection.getElementsByTagName("dc:identifier")
         identifier_value = identifier[i].firstChild.nodeValue
-        if identifier_value.startswith('https://'):
+        if identifier_value.startswith('http'):
             link_value = identifier_value               
             #get geometry from html
-            #identifier_url = os.path.join(os.getcwd(),'harvesting\\journal_1\\article_01.html')
             geom = get_geom(link_value)
             geom_data = geom["features"][0]["geometry"]
             geom_data_string = json.dumps(geom_data)
@@ -48,19 +49,33 @@ def harvest_data(url):
             type_geom.update(geom_content)
             geom_data = json.dumps(type_geom)
             # geometry field accept json object as string
-            geom_object = GEOSGeometry(geom_data) #GeometryCollection object
-            #bounds=MultiPolygon([Polygon(((-117.869537353516, 33.5993881225586),(-117.869537353516, 33.7736549377441),(-117.678024291992, 33.7736549377441),(-117.678024291992, 33.5993881225586),(-117.869537353516, 33.5993881225586)))])           
-            #gc = GeometryCollection(Point(0, 0), MultiPoint(Point(0, 0), Point(1, 1)), bounds)
-            title = collection.getElementsByTagName("dc:title")
-            title_value = title[0].firstChild.nodeValue
-            abstract = collection.getElementsByTagName("dc:description") 
-            abstract_text = abstract[0].firstChild.nodeValue
-            journal = collection.getElementsByTagName("dc:publisher")
-            journal_value = journal[0].firstChild.nodeValue
-            date = collection.getElementsByTagName("dc:date")
-            date_value = date[0].firstChild.nodeValue                   
-            publication = Publication(title = title_value,abstract = abstract_text,publicationDate = date_value, url = link_value , geometry = geom_object,journal = journal_value)
-            publication.save()
+            try :
+                geom_object = GEOSGeometry(geom_data) #GeometryCollection object
+            except :
+                print("Invalid Geometry")            
+        else:
+            link_value = None
+            geom_object = None
+
+        title = collection.getElementsByTagName("dc:title")
+        title_value = title[0].firstChild.nodeValue
+        abstract = collection.getElementsByTagName("dc:description") 
+        abstract_text = abstract[0].firstChild.nodeValue
+        journal = collection.getElementsByTagName("dc:publisher")
+        journal_value = journal[0].firstChild.nodeValue
+        date = collection.getElementsByTagName("dc:date")
+        date_value = date[0].firstChild.nodeValue                   
+        publication = Publication(title = title_value,abstract = abstract_text,publicationDate = date_value, url = link_value , geometry = geom_object,journal = journal_value)
+        publication.save()
+        
+
+def harvest_data(url):
+    try:
+        response = requests.get(url)
+        parse_xml(response.content)        
+    except requests.exceptions.RequestException as e:  
+        print ("The requested URL is invalid or has bad connection.Please change the URL")
+   
 
 
 # shell
