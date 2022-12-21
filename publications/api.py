9a72a62f01b9051f58e963c7f82ee2899dd7960f
Kirubamoh <nkirubamohan@gmail.com>
2022-12-21 04:00:46 +0100

    Add viewsets and api path to subscription Model #39

--- publications/api.py
+++ publications/api.py
@@ -2,9 +2,9 @@
 
 from rest_framework import routers
 
-from publications.viewsets import PublicationViewSet
+from publications.viewsets import PublicationViewSet,SubscriptionViewset
 
 router = routers.DefaultRouter()
 router.register(r"publications", PublicationViewSet)
-
+router.register(r"subscriptions",SubscriptionViewset)
 urlpatterns = router.urls
